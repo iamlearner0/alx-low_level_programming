70ab008e1acb115ace94d6824bad81e7e6e7c960
iamlearner0 <quadrisaheed001@gmail.com>
2022-04-12 15:53:42 -0700

    double number combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,6 +11,7 @@
  * The combinations of numbers should be printed in ascending order
  * You can only use the putchar function
  * You can only use putchar eight times maximum in your code
+ * You are not allowed to use any variable of type char
  * All your code should be in the main function
  * Return: 0
  */
